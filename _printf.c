377536ece357901be90a6f8d41d03a987eb3452b
PulengKgatlhane <pulengkgatlhane1973@gmail.com>
2023-04-25 11:47:32 -0700

    Partner2

--- _printf.c
+++ _printf.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * printf - Printf function
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -49,4 +51,18 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
